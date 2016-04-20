a92409f1ad51dbbcffc7f893e42a48e56f79569e
xxc740 <740415505@qq.com>
2016-04-20 14:59:54 +0800

    演示Session

---
+++ SessionTest/SessionTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionTest.Controllers
+{
+    [OutputCache(NoStore = true,Duration = 0)]      //不缓存数据
+    public class HomeController : Controller
+    {
+        public List<string> boxes = new List<string>() { "red", "green", "blue", "black", "gray", "yellow", "orange" }; 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetBox()
+        {
+            Thread.Sleep(10);
+            Random rnd = new Random();
+            int index = rnd.Next(0, boxes.Count);
+
+            return boxes[index];
+        }
+
+        public ActionResult StartSeesion()      //启用Session并存值
+        {
+            Session["Name"] = "Chris";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SessionTest/SessionTest/Controllers/SessionResolvedController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionTest.Controllers
+{
+    [OutputCache(NoStore = true,Duration = 0)]
+    public class SessionResolvedController : Controller
+    {
+        public List<string> boxes = new List<string>() { "red", "green", "blue", "black", "gray", "yellow", "orange" };
+
+        public string GetBox()
+        {
+            try
+            {
+                Thread.Sleep(10);
+                Random rnd = new Random();
+                int index = rnd.Next(0, boxes.Count);
+
+                return boxes[index];
+            }
+            catch (Exception e)
+            {
+                return "red";
+            }
+        }
+    }
+}
