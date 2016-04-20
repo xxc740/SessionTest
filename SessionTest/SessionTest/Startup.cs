b873363cb8e6c5caac4b8152a9825845d5cf6771
xxc740 <740415505@qq.com>
2016-04-20 13:36:45 +0800

    空MVC

---
+++ SessionTest/SessionTest/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SessionTest.Startup))]
+namespace SessionTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
